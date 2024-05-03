9c6d7485bd71b718eea75c48663a827895d0781e
Sharon Trillo Nchana <sharz_esp@outlook.com>
2024-05-03 10:52:27 +0100

    The following changes were to add my first test for the 'Staff' class. It includes code in the test file however the class file 'clsStaff' is currently empty at this stage.
    - Sharon

---
+++ Testing4/tstStaff.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //Creates a new instance of the Staff Class
+            clsStaff NewStaff = new clsStaff();
+
+            //Test to see that it exists
+            Assert.IsNotNull(NewStaff);
+        }
+    }
+}
