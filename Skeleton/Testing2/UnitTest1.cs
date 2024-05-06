01f87054d3c4fdd00517cb014cd533d855c62368
abbieshaw <P2651576@my365.dmu.ac.uk>
2024-05-06 21:42:59 +0100

    Added testing for orders class

--- Skeleton/Testing2/UnitTest1.cs
+++ Skeleton/Testing2/UnitTest1.cs
@@ -2,16 +2,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace Testing4
+namespace Testing2
 {
     [TestClass]
-    public class tstOrder
+    public class tstOrders
     {
         [TestMethod]
         public void InstanceOK()
         {
             //create an instance of the class we want to create
-            clsOrder OrderOne = new clsOrder();
+            clsOrders OrderOne = new clsOrders();
             //test to see that it exists
             Assert.IsNotNull(OrderOne);
         }
