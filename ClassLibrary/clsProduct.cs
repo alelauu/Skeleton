5a7bb51eeea4fe885ec34c3beda1cebe2f3a5850
alelauu <P2721763@my365.dmu.ac.uk>
2024-05-07 10:52:01 +0100

    Added more testing for Stock/Product class

--- ClassLibrary/clsProduct.cs
+++ ClassLibrary/clsProduct.cs
@@ -2,5 +2,13 @@
 {
     public class clsProduct
     {
+        public int Id { get; set; }
+        public bool InStock { get; set; }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductBrand { get; set; }
+        public string ProductColour { get; set; }
+        public string ProductCapacity { get; set; }
+        public int AmountInStock { get; set; }
     }
 }
