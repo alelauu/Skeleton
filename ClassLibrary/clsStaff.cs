b0c98b53819bf341bce4bc12868667c0a86e9471
Sharon Trillo Nchana <sharz_esp@outlook.com>
2024-05-07 10:35:02 +0100

    I have added the tests necessary for each field entry in my Staff database, by updating my 'tstStaff' class and I also updated the 'clsStaff' file to include get and set methods for different data ntries in my database.

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,18 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+
+        public bool IsOnline { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public string Number { get; set; }
+        public string PostCode { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string StaffId { get; set; }
     }
 }
--- Testing4/tstStaff.cs
+++ Testing4/tstStaff.cs
@@ -9,16 +9,143 @@ namespace Testing4
     {
         [TestMethod]
         public void InstanceOK()
-        { 
+        {
             //Creates an instanc eof the Staff Class
             clsStaff NewStaff = new clsStaff();
 
-        //Tests to see if it exists
-        Assert.IsNotNull(NewStaff);
+            //Tests to see if it exists
+            Assert.IsNotNull(NewStaff);
 
 
         }
+
+        [TestMethod]
+        public void IsOnlinePropertyOK()
+        {
+            //Creates a new instance of the Staff class
+
+            clsStaff NewStaff = new clsStaff();
+
+            //Creates some test data to assign to the property
+            Boolean TestStaff = true;
+
+            //Assign the data to the property
+            NewStaff.IsOnline = TestStaff;
+
+            //Tests to see that the values are equal
+            Assert.AreEqual(NewStaff.IsOnline, TestStaff);
+        }
+
+        [TestMethod]
+        public void StartDatePropertyOK()
+        {
+            //Creates a new instance of the Staff class
+
+            clsStaff NewStaff = new clsStaff();
+
+            //Creates some test data to assign to the property
+            DateTime TestStaff = DateTime.Now.Date;
+
+            //Assign the data to the property
+            NewStaff.StartDate = TestStaff;
+
+            //Tests to see that the values are equal
+            Assert.AreEqual(NewStaff.StartDate, TestStaff);
+        }
+
+
+        [TestMethod]
+        public void NumberPropertyOK()
+        {
+            //Creates a new instance of the Staff class
+
+            clsStaff NewStaff = new clsStaff();
+
+
+            //Creates some test data to assign to the property
+            String TestStaff = "07497070472";
+
+            //Assign the data to the property
+            NewStaff.Number = TestStaff;
+
+            //Tests to see that the values are equal
+            Assert.AreEqual(NewStaff.Number, TestStaff);
+        }
+
+        [TestMethod]
+        public void PostCodePropertyOK()
+        {
+            //Creates a new instance of the Staff class
+
+            clsStaff NewStaff = new clsStaff();
+
+
+            //Creates some test data to assign to the property
+            String TestStaff = "LE1 5AN";
+
+            //Assign the data to the property
+            NewStaff.PostCode = TestStaff;
+
+            //Tests to see that the values are equal
+            Assert.AreEqual(NewStaff.PostCode, TestStaff);
+        }
+
+
+        [TestMethod]
+        public void EmailPropertyOK()
+        {
+            //Creates a new instance of the Staff class
+
+            clsStaff NewStaff = new clsStaff();
+
+
+            //Creates some test data to assign to the property
+            String TestStaff = "P2765365@my365.dmu.ac.uk";
+
+            //Assign the data to the property
+            NewStaff.Email = TestStaff;
+
+            //Tests to see that the values are equal
+            Assert.AreEqual(NewStaff.Email, TestStaff);
+        }
+
+
+        [TestMethod]
+        public void NamePropertyOK()
+        {
+            //Creates a new instance of the Staff class
+
+            clsStaff NewStaff = new clsStaff();
+
+
+            //Creates some test data to assign to the property
+            String TestStaff = "Abbie";
+
+            //Assign the data to the property
+            NewStaff.Name = TestStaff;
+
+            //Tests to see that the values are equal
+            Assert.AreEqual(NewStaff.Name, TestStaff);
+        }
+
+        [TestMethod]
+        public void StaffIdPropertyOK()
+        {
+            //Creates a new instance of the Staff class
+
+            clsStaff NewStaff = new clsStaff();
+
+
+            //Creates some test data to assign to the property
+            String TestStaff = "2";
+
+            //Assign the data to the property
+            NewStaff.StaffId = TestStaff;
+
+            //Tests to see that the values are equal
+            Assert.AreEqual(NewStaff.StaffId, TestStaff);
+        }
     }
-}
+    }
 
 
