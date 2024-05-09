38bb32fa03c8ee51793223077b5bc1cd8548c7dd
alelauu <P2721763@my365.dmu.ac.uk>
2024-05-09 18:16:56 +0100

    Added a presentation layer where it displays user input

--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,16 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //creating a new instance of clsProduct
+        clsProduct ProductOne = new clsProduct();
+        //getting the data from the session object
+        ProductOne = (clsProduct)Session["ProductOne"];
+        //display the product name for this entry
+        Response.Write(ProductOne.ProductName);
+        Response.Write(ProductOne.ProductBrand);
+        Response.Write(ProductOne.ProductColour);
+        Response.Write(ProductOne.ProductCapacity);
+        Response.Write(ProductOne.AmountInStock);
+        Response.Write(ProductOne.InStock);
     }
 }
