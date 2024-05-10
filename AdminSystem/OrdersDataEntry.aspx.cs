18d68eb0f70421ddde69c438bb0538032aebc7aa
abbieshaw <P2651576@my365.dmu.ac.uk>
2024-05-10 10:46:20 +0100

    Orders Viewer updates

--- AdminSystem/OrdersDataEntry.aspx.cs
+++ AdminSystem/OrdersDataEntry.aspx.cs
@@ -4,16 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        
     }
+
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
+        //create a new instance of clsOrder
+        clsOrder OrderOne = new clsOrder();
+        //capture the order status
+        OrderOne.OrderStatus = txtOrderStatus.Text;
+        //store the order in the session object
+        Session["OrderOne"] = OrderOne;
         //navigate to the view page
         Response.Redirect("OrdersViewer.aspx");
     }
