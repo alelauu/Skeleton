569f0db637b1d74c91ef323129764aa01301e2ef
P2718902 <marianigbinidu123>
2024-05-21 14:19:44 +0100

    test

---
+++ Testing3/tstCustomer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class wwe want to create 
+            clsCustomer ACustomer = new clsCustomer();
+            //test  to see that it exists 
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
