b11ebfe226217a70cf239ced411d6db02e79978b
P2765458 <P2765458@CLPX0027WIN064W>
2024-05-21 15:04:18 +0100

    P7

--- Testing5/UnitTest1.cs
+++ Testing5/UnitTest1.cs
@@ -15,5 +15,31 @@ namespace Testing5
             //test to see that it exists
             Assert.IsNotNull(AnSupplier);
         }
+        [TestMethod]
+
+        public void ActivePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //test to see that it exists
+            Assert.IsNotNull(AnSupplier);
+        }
+        [TestMethod]
+
+        public void ActivePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create some test data to assign to the property
+            boolean TestData = true;
+            //assign the data to the property
+            AnSupplier.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnSupplier.Active, TestData);
+        }
+
+ 
+      
+
     }
 }
