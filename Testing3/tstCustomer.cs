421b8fb0b280dd9ff89e7459f60ffe19265f4545
P2718902 <marianigbinidu123>
2024-05-23 22:58:12 +0100

    finished p6 lol

--- Testing3/tstCustomer.cs
+++ Testing3/tstCustomer.cs
@@ -10,7 +10,9 @@ namespace Testing3
         [TestMethod]
         public void InstanceOk()
         {
+            //create an instance of the class we want to create
             clsCustomer AnCustomer = new clsCustomer();
+            //test to see that it exists 
             Assert.IsNotNull(AnCustomer);
 
         }
