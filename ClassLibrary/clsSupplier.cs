7dbd890be5ceadbc3248ce1d7775058554fda187
P2765458 <P2765458@CLPX0027WIN064W>
2024-05-28 15:02:29 +0100

    practical 7

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,6 +1,13 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSupplier
     {
+        public bool Active { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int SupplierId { get; set; }
+        public int Postcode { get; set; }
+        public string Address { get; set; }
     }
 }
--- Testing5/UnitTest1.cs
+++ Testing5/UnitTest1.cs
@@ -15,31 +15,106 @@ namespace Testing5
             //test to see that it exists
             Assert.IsNotNull(AnSupplier);
         }
+
         [TestMethod]
-
-        public void ActivePropertyOK()
-        {
-            //create an instance of the class we want to create
-            clsSupplier AnSupplier = new clsSupplier();
-            //test to see that it exists
-            Assert.IsNotNull(AnSupplier);
-        }
-        /*[TestMethod]
-
         public void ActivePropertyOK()
         {
             //create an instance of the class we want to create
             clsSupplier AnSupplier = new clsSupplier();
             //create some test data to assign to the property
-            boolean TestData = true;
-            //assign the data to the property
+            Boolean TestData = true;
+            //assign data to the property 
             AnSupplier.Active = TestData;
-            //test to see that the two values are the same
+            //test to see that the two values are the same 
             Assert.AreEqual(AnSupplier.Active, TestData);
-        }*/
+        }
+        [TestMethod]
+        public void QualityAssurancePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create some test data to assign to the property 
+            Boolean TestData = true;
+            //assign the data to the property 
+            AnSupplier.Active = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnSupplier.Active, TestData);
+        }
 
- 
-      
+        [TestMethod]
+        public void DeliveryDatePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnSupplier.DeliveryDate = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnSupplier.DeliveryDate, TestData);
+        }
 
-    }
-}
+        [TestMethod]
+        public void QuantityOrderedPropertyOK()
+        {
+            //create an instance of the class we want to create 
+            clsSupplier AnSupplier = new clsSupplier();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnSupplier.SupplierId = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnSupplier.SupplierId, TestData);
+        }
+
+        [TestMethod]
+        public void PostcodePropertyOK()
+        {
+            //create an instance of the class we want to create 
+            clsSupplier AnSupplier = new clsSupplier();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnSupplier.Postcode = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnSupplier.Postcode, TestData);
+        }
+
+        [TestMethod]
+        public void AddressPropertyOK()
+        {
+            //create an instance of the class we want to create 
+            clsSupplier AnSupplier = new clsSupplier();
+            //create some test data to assign to the property
+            string TestData = "21b";
+            //assign the data to the property
+            AnSupplier.Address = TestData;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AnSupplier.Address, TestData);
+        }
+
+        [TestMethod]
+        public void ContactPhonePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier clsSupplier = new clsSupplier();
+            //create some test data to assign to the property
+            string TestData = "072632762";
+            //assign the data to the property
+            AnSupplier.ContactPhone = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnSupplier.ContactPhone, TestData);
+
+
+
+        }
+
+            ]
+
+        }
+
+
+
+        
+    
+
