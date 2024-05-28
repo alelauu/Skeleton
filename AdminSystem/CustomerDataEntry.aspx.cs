eb9d81fc3a60e3fe22bff03cabb21c4c8a0a36e2
P2718902 <marianigbinidu123>
2024-05-28 16:07:46 +0100

    Almost done with pratical 8

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -11,4 +11,30 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
 
     }
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox4_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox5_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox7_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //navigate to the view page 
+        Response.Redirect("CustomerBookViewer.aspx");
+    }
 }
