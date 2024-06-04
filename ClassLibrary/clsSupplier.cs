9acbd37c3918807a7867adb68e9bdf1a886e671c
P2765458 <P2765458@CLPX0027WIN064W>
2024-06-04 13:00:42 +0100

    prac 9a

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -1,12 +1,207 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace ClassLibrary
 {
     public class clsSupplier
     {
+        //private data member for the SupplierId property
+        private Int32 mSupplierId;
+        //supplierId public property
+        public Int32 SupplierId
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mSupplierId;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mSupplierId = value;
+            }
+        }
+
+        //private data member for the deliveryDate property
+        private DateTime mDeliveryDate;
+        //deliveryDate public property
+        public DateTime DeliveryDate
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mDeliveryDate;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mDeliveryDate = value;
+            }
+        }
+
+        //private data member for the QuantityOrdered property
+        private Int32 mQuantityOrdered;
+        //QuantityOrdered public property
+        public Int32 QuantityOrdered
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mQuantityOrdered;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mQuantityOrdered = value;
+            }
+        }
+
+        //private data member for the active property
+        private Boolean mActive;
+        //active public property
+        public bool Active
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mActive;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mActive = value;
+            }
+        }
+
+        //private data member for the QualityAssurance property
+        private Boolean mQualityAssurance;
+        //QualityAssurance public property
+        public bool QualityAssurance
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mQualityAssurance;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mQualityAssurance = value;
+            }
+        }
+
+        //private data member for the post code property
+        private string mPostCode;
+        //post code public property
+        public string PostCode
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mPostCode;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mPostCode = value;
+            }
+        }
+
+        //private data member for the Address property
+        private string mAddress;
+        //Address public property
+        public string Address
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mAddress;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mAddress = value;
+            }
+        }
+
+        //private data member for the ContactPhone property
+        private string mContactPhone;
+        //ContactPhone public property
+        public string ContactPhone
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mContactPhone;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mContactPhone = value;
+            }
+        }
+
+        //private data member for the Email property
+        private string mEmail;
+        //Email public property
+        public string Email
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mEmail;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mEmail = value;
+            }
+        }
+
+        //private data member for the post code property
+        private string mCompanyName;
+        //post code public property
+        public string CompanyName
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mCompanyName;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mCompanyName = value;
+            }
+        }
+
+        public string Postcode { get; set; }
+
+        public bool Find(int supplierId)
+        {
+            //set the private data members to the test data value
+            mSupplierId = 21;
+            mDeliveryDate = Convert.ToDateTime("23/12/2022");
+            mQuantityOrdered = 67;
+            mActive = true;
+            mQualityAssurance = true;
+            mPostCode = "le34pa";
+            mAddress = "535 st saviours road";
+            mContactPhone = "07865491211";
+            mEmail = "sadjsa@gmail.com";
+            mCompanyName = "ajsdjashd";
+            //always return true
+            return true;
+        }
+
+
+
+
+
+/*
         public bool Active { get; set; }
         public DateTime DeliveryDate { get; set; }
-        public int SupplierId { get; set; }
+        public int supplierId { get; set; }
         public string Postcode { get; set; }
         public string Address { get; set; }
         public int QuantityOrdered { get; set; }
@@ -14,5 +209,8 @@ namespace ClassLibrary
         public string ContactPhone { get; set; }
         public string Email { get; set; }
         public string CompanyName { get; set; }
+       
+
+      */
     }
 }
--- Testing5/tstSupplier.cs
+++ Testing5/tstSupplier.cs
@@ -8,6 +8,7 @@ namespace Testing5
     public class tstSupplier
     {
         public object AnSupplier { get; private set; }
+        public object SupplierId { get; private set; }
 
         [TestMethod]
         public void InstanceOK()
@@ -38,7 +39,7 @@ namespace Testing5
             //create some test data to assign to the property 
             Boolean TestData = true;
             //assign the data to the property 
-            AnSupplier.Active = TestData;
+            AnSupplier.QualityAssurance = TestData;
             //test to see that the two values are the same 
             Assert.AreEqual(AnSupplier.QualityAssurance, TestData);
         }
@@ -64,7 +65,7 @@ namespace Testing5
             //create some test data to assign to the property
             Int32 TestData = 8;
             //assign the data to the property
-            AnSupplier.SupplierId = TestData;
+            AnSupplier.QuantityOrdered = TestData;
             //test to see that the two values are the same 
             Assert.AreEqual(AnSupplier.QuantityOrdered, TestData);
         }
@@ -134,16 +135,249 @@ namespace Testing5
             Assert.AreEqual(AnSupplier.CompanyName, TestData);
         }
 
-
-
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create a Boolean variable to store the results of the validation
+            Boolean Found = false;
+            //create some test data to use with the method
+            Int32 SupplierId = 21;
+            //invoke the method
+            Found = AnSupplier.Find(SupplierId);
+            //test to see if the result is true
+            Assert.IsTrue(Found);
         }
 
-            
 
+        [TestMethod]
+        public void TestSupplierIdOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create a Boolean variable to store the results of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SupplierId = 21;
+            //invoke the method
+            Found = AnSupplier.Find(SupplierId);
+            //check the supplier id
+            if (AnSupplier.SupplierId != 21)
+            {
+                OK = false;
+            }
+            //test to see if the result is correct
+            Assert.IsTrue(OK);
         }
 
+        [TestMethod]
+        public void TestPostcodeOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create a Boolean variable to store the results of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SupplierId = 21;
+            //invoke the method
+            Found = AnSupplier.Find(SupplierId);
+            //check the Postcode property
+            if (AnSupplier.Postcode != "le34pa")
+            {
+                OK = false;
+            }
+            //test to see if the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestAddressOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create a Boolean variable to store the results of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SupplierId = 21;
+            //invoke the method
+            Found = AnSupplier.Find(SupplierId);
+            //check the Address property
+            if (AnSupplier.Address != "535 st saviours road")
+            {
+                OK = false;
+            }
+            //test to see if the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestContactPhoneOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create a Boolean variable to store the results of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SupplierId = 21;
+            //invoke the method
+            Found = AnSupplier.Find(SupplierId);
+            //check the ContactPhone property
+            if (AnSupplier.ContactPhone != "07865491211")
+            {
+                OK = false;
+            }
+            //test to see if the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestEmailOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create a Boolean variable to store the results of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SupplierId = 21;
+            //invoke the method
+            Found = AnSupplier.Find(SupplierId);
+            //check the Email property
+            if (AnSupplier.Email != "sadjsa@gmail.com")
+            {
+                OK = false;
+            }
+            //test to see if the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestCompanyNameOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create a Boolean variable to store the results of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SupplierId = 21;
+            //invoke the method
+            Found = AnSupplier.Find(SupplierId);
+            //check the CompanyName property
+            if (AnSupplier.CompanyName != "ajsdjashd")
+            {
+                OK = false;
+            }
+            //test to see if the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestQuantityOrderedOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create a Boolean variable to store the results of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SupplierId = 21;
+            //invoke the method
+            Found = AnSupplier.Find(SupplierId);
+            //check the QuantityOrdered property
+            if (AnSupplier.QuantityOrdered != 67)
+            {
+                OK = false;
+            }
+            //test to see if the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestDeliveryDateFound()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create a Boolean variable to store the result of the search
+            Boolean Found = false;
+            //create Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SupplierId = 21;
+            //invoke the method
+            Found = AnSupplier.Find(SupplierId);
+            //check the DeliveryDate property
+            if (AnSupplier.DeliveryDate != Convert.ToDateTime("12/08/2024"))
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestQualityAssuranceFound()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create a Boolean variable to store the result of the search
+            Boolean Found = false;
+            //create Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SupplierId = 21;
+            //invoke the method
+            Found = AnSupplier.Find(SupplierId);
+            //check the QualityAssurance property
+            if (AnSupplier.QualityAssurance != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestActiveFound()
+        {
+            //create an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //create a Boolean variable to store the result of the search
+            Boolean Found = false;
+            //create Boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 SupplierId = 21;
+            //invoke the method
+            Found = AnSupplier.Find(SupplierId);
+            //check the active property
+            if (AnSupplier.Active != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+    }
+}
 
 
-        
-    
+
+
+
+
+
+
 
