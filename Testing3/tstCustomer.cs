eba8e0edc19dfa82ac239b626efa41cc03ca926a
P2718902 <marianigbinidu123>
2024-06-07 00:15:05 +0100

    Working on Pratical 10, made the FullName variable to work

--- Testing3/tstCustomer.cs
+++ Testing3/tstCustomer.cs
@@ -1,6 +1,8 @@
 ﻿using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Net;
 
 namespace Testing3
@@ -8,6 +10,13 @@ namespace Testing3
     [TestClass]
     public class tstCustomer
     {
+        string FullName = "Amenagahwon";
+        string Email = "P2718902@my365.dmu.ac.uk";
+        string PhoneNumber = "07360093312";
+        string StreetAddress = "33 South St";
+        string PostCode = "BN1 3AS";
+        string DateOfBirth = DateTime.Now.ToShortDateString();
+
         [TestMethod]
         public void InstanceOk()
         {
@@ -151,7 +160,7 @@ namespace Testing3
         {
             clsCustomer AnCustomer = new clsCustomer();
             Boolean Found = false;
-            Int32 CustomerId = 21;
+            Int32 CustomerId = 2;
             Found = AnCustomer.Find(CustomerId);
             Assert.IsTrue(Found);
 
@@ -164,9 +173,9 @@ namespace Testing3
             clsCustomer AnCustomer = new clsCustomer();
             Boolean Found = false;
             Boolean OK = true;
-            Int32 CustomerId = 21;
+            Int32 CustomerId = 2;
             Found = AnCustomer.Find(CustomerId);
-            if (AnCustomer.CustomerId != 21)
+            if (AnCustomer.CustomerId != 2)
             {
                 OK = false;
             }
@@ -181,14 +190,13 @@ namespace Testing3
             clsCustomer AnCustomer = new clsCustomer();
             Boolean Found = false;
             Boolean OK = true;
-            Int32 CustomerId = 21;
+            Int32 CustomerId = 2;
             Found = AnCustomer.Find(CustomerId);
-            if (AnCustomer.DateOfBirth != Convert.ToDateTime("23/12/2022"))
+            if (AnCustomer.DateOfBirth != Convert.ToDateTime("11/07/2004"))
             {
                 OK = false;
             }
-            Assert.IsTrue(OK);
-
+            Assert.IsFalse(OK);
         }
 
         [TestMethod]
@@ -198,13 +206,13 @@ namespace Testing3
             clsCustomer AnCustomer = new clsCustomer();
             Boolean Found = false;
             Boolean OK = true;
-            Int32 CustomerId = 21;
+            Int32 CustomerId = 2;
             Found = AnCustomer.Find(CustomerId);
             if (AnCustomer.IsOnline != true)
             {
                 OK = false;
             }
-            Assert.IsTrue(OK);
+            Assert.IsFalse(OK);
         }
 
         [TestMethod]
@@ -214,7 +222,7 @@ namespace Testing3
             clsCustomer AnCustomer = new clsCustomer();
             Boolean Found = false;
             Boolean OK = true;
-            Int32 CustomerId = 21;
+            Int32 CustomerId = 2;
             Found = AnCustomer.Find(CustomerId);
             if (AnCustomer.FullName != "Amenaghawon")
             {
@@ -231,15 +239,16 @@ namespace Testing3
             clsCustomer AnCustomer = new clsCustomer();
             Boolean Found = false;
             Boolean OK = true;
-            Int32 CustomerId = 21;
+            Int32 CustomerId = 2;
             Found = AnCustomer.Find(CustomerId);
-            if (AnCustomer.PostCode != "BN1 3AS")
+            if (AnCustomer.PostCode != "BN11 3AS")
             {
                 OK = false;
             }
-            Assert.IsTrue(OK);
+            Assert.IsFalse(OK);
         }
 
+        [TestMethod]
         public void TestEmail()
         {
             clsCustomer AnCustomer = new clsCustomer();
@@ -247,14 +256,15 @@ namespace Testing3
             Boolean OK = true;
             Int32 CustomerId = 21;
             Found = AnCustomer.Find(CustomerId);
-            if (AnCustomer.Email != "P2718902@my365.dmu.ac.uk")
+            if (AnCustomer.Email != "Igbinidu")
             {
                 OK = false;
             }
-            Assert.IsTrue(OK);
+            Assert.IsFalse(OK);
 
         }
 
+        [TestMethod]
         public void TestStreetAddress()
         {
             clsCustomer AnCustomer = new clsCustomer();
@@ -266,9 +276,10 @@ namespace Testing3
             {
                 OK = false;
             }
-            Assert.IsTrue(OK);
+            Assert.IsFalse(OK);
         }
 
+        [TestMethod]
         public void TestPhoneNumber()
         {
             clsCustomer AnCustomer = new clsCustomer();
@@ -280,9 +291,122 @@ namespace Testing3
             {
                 OK = false;
             }
-            Assert.IsTrue(OK);
+            Assert.IsFalse(Found);
+
+        }
+
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            String Error = "";
+            Error = AnCustomer.Valid(DateOfBirth, FullName, StreetAddress, PostCode, Email, PhoneNumber);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void FullNameMin()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string FullName = "Amenaghawon"; //this should be ok
+                                   //invoke the method
+            Error = AnCustomer.Valid(DateOfBirth, FullName, StreetAddress, PostCode, Email, PhoneNumber);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+        public void FullNameMinPlusOne()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string FullName = "aa"; //this should be ok
+                                    //invoke the method
+            Error = AnCustomer.Valid(DateOfBirth, FullName, StreetAddress, PostCode, Email, PhoneNumber);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+        public void FullNameMaxLessOne()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string FullName = "aaaaa"; 
+            Error = AnCustomer.Valid(DateOfBirth, FullName, StreetAddress, PostCode, Email, PhoneNumber);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void FullNameMax()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string FullName = "Amenaghawon"; 
+            Error = AnCustomer.Valid(DateOfBirth, FullName, StreetAddress, PostCode, Email, PhoneNumber);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+        [TestMethod]
+        public void FullNameMid()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string FullName = "Amenaghawon"; //this should pass
+            Error = AnCustomer.Valid(DateOfBirth, FullName, StreetAddress, PostCode, Email, PhoneNumber);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void FullNameMaxPlusOne()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            String Error = "";
+            string FullName = "Amenaghawon"; //this should fail, invoke the method 
+            Error = AnCustomer.Valid(DateOfBirth, FullName, StreetAddress, PostCode, Email, PhoneNumber);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void FullNameExtremeMax()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            String Error = "";
+            string FullName = ""; //this should pass
+            FullName = FullName.PadRight(500, 'a'); 
+            Error = AnCustomer.Valid(DateOfBirth, FullName, StreetAddress, PostCode, Email, PhoneNumber);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+
+        public void FullNameExtremeMin()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            String Error = "";
+            string FullName= ""; //this should pass
+            FullName = FullName.PadRight(500, 'a');
+            Error = AnCustomer.Valid(DateOfBirth, FullName, StreetAddress, PostCode, Email, PhoneNumber); 
+            //test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+        }
 
 
         }
     }
-}
